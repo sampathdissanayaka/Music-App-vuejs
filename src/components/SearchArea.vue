df209460183913c709057d909f5bb97d19c4227b
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-25 13:50:41 +0700

    create searchArea page

---
+++ src/components/SearchArea.vue
@@ -0,0 +1,14 @@
+<template>
+
+      <div class="form-group ">
+            <label for="inputEmail" class="col-lg-2 control-label">Search Music</label>
+            <div class="col-lg-10">
+                <input type="text" class="form-control" id="inputEmail" placeholder="Type here..">
+            </div>
+      </div>
+
+</template>
+
+<script></script>
+
+<style></style>
