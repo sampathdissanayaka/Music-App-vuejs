7832fc7005ba79c58edd9042b77e568c8b3789d0
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-25 11:59:48 +0700

    create homevue

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+
+<form>
+<caption></caption>
+  <fieldset>
+    <legend>Music App</legend>
+    <div class="form-group ">
+      <label for="inputEmail" class="col-lg-2 control-label">Search Music</label>
+      <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputEmail" placeholder="Type here..">
+      </div>
+    </div>
+   
+  </fieldset>
+</form>
+
+</div>
+
+</template>
+
+<script></script>
+
+<style></style>
