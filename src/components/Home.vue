7ec26b28e3106523f862aa56b05fdb5c90f644ba
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-25 12:25:33 +0700

    update home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,19 @@
 <template>
 <div class="container">
 
-<form>
-<caption></caption>
-  <fieldset>
-    <legend>Music App</legend>
-    <div class="form-group ">
-      <label for="inputEmail" class="col-lg-2 control-label">Search Music</label>
-      <div class="col-lg-10">
-        <input type="text" class="form-control" id="inputEmail" placeholder="Type here..">
-      </div>
-    </div>
-   
-  </fieldset>
-</form>
+
+        <form>
+        <fieldset>
+            <legend>Music App</legend>
+            <div class="form-group ">
+            <label for="inputEmail" class="col-lg-2 control-label">Search Music</label>
+            <div class="col-lg-10">
+                <input type="text" class="form-control" id="inputEmail" placeholder="Type here..">
+            </div>
+            </div>
+        </fieldset>
+        </form>
+
 
 </div>
 
