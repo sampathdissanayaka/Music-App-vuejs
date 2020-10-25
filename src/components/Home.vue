a1db19660c28bfa3a124298c95e43bb2acd02c11
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-25 13:49:37 +0700

    update homevue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,12 +5,9 @@
         <form>
         <fieldset>
             <legend>Music App</legend>
-            <div class="form-group ">
-            <label for="inputEmail" class="col-lg-2 control-label">Search Music</label>
-            <div class="col-lg-10">
-                <input type="text" class="form-control" id="inputEmail" placeholder="Type here..">
-            </div>
-            </div>
+            
+            <app-search></app-search>
+
         </fieldset>
         </form>
 
@@ -23,6 +20,16 @@
 
 </template>
 
-<script></script>
+<script>
+
+import SearchArea from './SearchArea.vue'
+
+export default {
+  components:{
+    'app-search':SearchArea
+    }
+  }
+
+</script>
 
 <style></style>
